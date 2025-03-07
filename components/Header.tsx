e908f9d0bd776cb32ae56fb2c290052dc1d23ebf
kuramentooo <clement.delaunoy@supdevinci-edu.fr>
2025-03-07 15:28:54 +0100

    MOD : all

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image';
+
 const Header: React.FC = () => {
   return (
-    <header className="bg-primary text-white p-4 shadow-md">
-      <h1 className="text-4xl font-bold text-center">Mini Wiki Anime/Manga</h1>
+    <header className="bg-primary text-white p-4 shadow-md flex items-center">
+      <div className="flex items-center">
+        <Image src="/images/logo.png" alt="Logo" width={50} height={50} className="mr-4" />
+        <h1 className="text-4xl font-bold">Mini Wiki Anime/Manga</h1>
+      </div>
     </header>
   );
 };
